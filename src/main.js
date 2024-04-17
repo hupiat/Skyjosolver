9a750ecf57c4500b01824343e94537a9e92ac8e4
Hugo Piat <h.piat@outlook.fr>
2024-04-17 13:11:50 +0200

    update src/main.js

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ const grid = function() {
   const LINE_SIZE = 3;
   const possibleValues = [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   
-  const grid = [LINE_SIZE][LINE_SIZE];
+  const grid = Int8Array(LINE_SIZE);
   
   const init = () => {
     for (const i = 0; i <= LINE_SIZE; i++) {
       for (const j = 0; j <= LINE_SIZE; j++) {
-        const rand = getRandom(possibleValues[0], possibleValues[possibleValues.length - 1]);
+        const rand = getRandom(possibleValues[0], possibleValues[possibleValue  s.length - 1]);
         grid[i][j] = rand;
       }
     }
